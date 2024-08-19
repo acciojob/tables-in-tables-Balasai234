686dd2a9a6a3afe85c5beb75980e84df9bc7ae89
Balasai234 <balasaicheni7@gmail.com>
2024-08-19 11:52:06 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-describe('My Test', () => {
-  it('finds "Column 1"', () => {
-    cy.visit('http://localhost:3000') // replace with your URL
-    cy.contains('Column 1', { timeout: 10000 })
-  })
-})
